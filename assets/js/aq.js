4f7946ef1a6ff41c22863f9482267f2775fee732
Mauro Filho <mauro.filho@gmail.com>
2017-07-08 17:11:01 -0300

    google places api

--- assets/js/aq.js
+++ assets/js/aq.js
@@ -1,32 +1,8 @@
 (function($) {
-    const MAPS_API = 'https://maps.googleapis.com/maps/api/place/textsearch/json?type=cities&key=AIzaSyBNMq75Gp3PZMrntv-Mk30E9Z8Aq4X-DNI';
-    var app = {
-      init : function() {
-        $('#location_input').on('keypress', app._findCities);
-      },
-      _findCities: function(e) {
-          var query = e.target.value;
-          if (query.length > 2) {
-              $.ajax( {
-                  url: MAPS_API + '&query=' + e.target.value + '+in+brasil',
-                  beforeSend: function(xhr) {
-                      xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-                  },
-                  crossDomain: true
-              })
-              .done(function(data) {
-                  $('#cities').empty();
-                  for (var i in data.results) {
-                      $('#cities').append('<option value="'+data.results[i].formatted_address+'">');
-                  }
-              })
-              .fail(function(e) {
-                throw e;
-              });
-          }
-      }
+    var input = $('#location_input')[0];
+    var options = {
+      types: ['(cities)'],
+      componentRestrictions: {country: 'br'}
     };
-    $(document).ready(function() {
-      app.init();
-    });
+    autocomplete = new google.maps.places.Autocomplete(input, options);
 })(jQuery);
